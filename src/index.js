90992d760b159e3e646b4e10d6b2d4ea2b62ea2c
TP-WenpinLiao <wenpin.liao@tpisoftware.com>
2020-10-28 14:53:02 +0800

    add form validate effect

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './assets/scss/index.scss'
 import 'bootstrap'
+import 'jquery-validation'
 
 document.addEventListener('DOMContentLoaded', function () {
   // document ready
@@ -24,5 +25,51 @@ document.addEventListener('DOMContentLoaded', function () {
   $label.text($select.find('option:selected').text())
   $(document).on('change', '.ngchat-select > select', function (event) {
     $(this).parent().find('.ngchat-select__label').text($(this).find('option:selected').text())
+  })
+
+  $('#userForm').validate({
+    rules: {
+      name: {
+        required: true
+      },
+      country: {
+        required: true
+      },
+      email: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      name: {
+        required: '*Please enter a name.'
+      },
+      country: {
+        required: '*Please select a country.'
+      },
+      email: {
+        required: '*Please enter a valid company email.',
+        email: '*Please enter a valid company email.'
+      }
+    },
+    errorPlacement: function (error, element) {
+      if (element.attr('name') === 'country') {
+        error.insertAfter(element.parent())
+      } else {
+        error.insertAfter(element)
+      }
+    },
+    highlight: function (element, errorClass) {
+      $(element).removeClass(errorClass)
+    },
+    errorClass: 'error-message',
+    errorElement: 'span',
+    submitHandler: function (form) {
+      // some other code
+      // maybe form submit or ajax
+    }
+  })
+  $('select').on('change', function () {
+    $('form').validate().element('select')
   })
 })
